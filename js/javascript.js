d6ddd9859a9d0e2cb09f4e95390aaf72732b51ff
Samuel <samuelruizmartin2412@gmail.com>
2026-03-29 21:43:22 +0200

    Limpieza de codigo y mejora

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,13 @@
 "use strict";
 
+let contadorChistes = 0;
+const selectCategoria = document.getElementById("categoria");
+const botonObtener = document.querySelector(".boton-obtener-chiste");
+const botonAleatorio = document.querySelectorAll("#aleatorio");
+const botonCopiar = document.querySelector("#copiar");
+const textoChiste = document.querySelector(".chiste-texto");
+const contador = document.querySelector("#contador");
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("La página ha cargado");
     init();
@@ -7,14 +15,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Función para inicializar los eventos y la lógica de la aplicación
 function init() {
-    const selectCategoria = document.getElementById("categoria");
-    const botonObtener = document.querySelector(".boton-obtener-chiste");
-    const botonAleatorio = document.querySelectorAll(".aleatorio");
-    const botonCopiar = document.querySelector(".copiar");
-    const textoChiste = document.querySelector(".chiste-texto");
-    const contador = document.querySelector(".contenedor-contador-chistes");
-
-
     botonAleatorio.forEach((boton) => {
         boton.addEventListener("click", () => {
             console.log("Botón aleatorio clicado");
@@ -27,17 +27,25 @@ function init() {
     });
 };
 
+// Función para obtener un chiste aleatorio de la API de Chuck Norris
 function chisteRandom(textoChiste) {
     fetch("https://api.chucknorris.io/jokes/random")
         .then((response) => response.json())
         .then((data) => {
             console.log(data.value);
             textoChiste.textContent = data.value;
+            actualizarContador();
         })
         .catch((error) => {
             console.error("Error al obtener el chiste:", error);
         });
-    
+
+}
+
+// Función para actualizar el contador de chistes obtenidos
+function actualizarContador() {
+    contadorChistes++;
+    contador.textContent = `Chistes obtenidos: ${contadorChistes}`;
 }
 
 // const x = fetch("https://api.chucknorris.io/jokes/categories")
